60c75075eee0294a001e1c51c668b4bb06956b2c
Korcan Aydin <aydinmuminkorcan@gmail.com>
2020-12-04 01:03:39 +0300

    :pencil: Use strict mode

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
+"use strict";
+
+const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URI;
 
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true
+	useNewUrlParser: true,
+	useCreateIndex: true,
 });
